490700eca5eb8aa54d7f4a836af578cd5fafb793
Arthur Medeiros <rthurmedeiros@gmail.com>
2020-05-30 22:08:02 -0300

    Socket simple error handling

--- src/socket/message.js
+++ src/socket/message.js
@@ -4,15 +4,29 @@ const Message = require('../models/message');
 module.exports = function (io, socket) {
   socket.on('write', (payload) => {
     jwt.verify(payload.token, process.env.SECRET, function(err, decoded) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (decoded["payload"] == null) {
+        console.log("TIMEOUT: UNABLED TO READ USER");
+        return;
+      }
       Message.create({
         text: payload.text,
         author: decoded.payload._id
       }, (err, message) => {
-        if (err) console.log(err);
+        if (err) {
+          console.log(err);
+          return;
+        }
         Message.findById(message._id)
           .populate('author')
           .exec(function (err, message) {
-            if (err) console.log(err);
+            if (err) {
+              console.log(err);
+              return;
+            }
             io.emit('message', message);
           })
       })
